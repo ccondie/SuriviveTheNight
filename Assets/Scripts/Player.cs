cc8bcbd2e5b616704f806942ed17b27cc5188ea5
Andrew Manwaring <Andrew Manwaring>
2017-02-25 14:43:57 -0700

    added mouse clicking to player movement. needs some improvement - for example, if a wall is anywhere in the path of the player's click, it says the player can't move there (instead of just modifying the player's path to move around the wall)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,36 +6,39 @@ namespace SurviveTheNight {
 
 	public class Player : MovingObject {
 
-		private int input_lag_def = 5;
-		private int input_lag_count = 0;
-
 		// Use this for initialization
 		protected override void Start () {
 			base.Start ();
 		}
 
 		private void OnDisable() {}
-		
-		// Update is called once per frame
-		void Update () {
+
+        private int worldToTile(float position) {
+            return (int)((position + 0.3) / 0.6);
+        }
+
+        // Update is called once per frame
+        void Update () {
 			int horizontal = 0;
 			int vertical = 0;
 
-			if (!isMoving && input_lag_count < 0) {
-				horizontal = (int)Input.GetAxisRaw ("Horizontal");
-				vertical = (int)Input.GetAxisRaw ("Vertical");
-				//if (horizontal != 0)
-				//	vertical = 0;
-				//if (horizontal != 0 || vertical != 0)
-				AttemptMove<Wall> (horizontal, vertical);
-				input_lag_count = input_lag_def;
-			} else
-				input_lag_count--;
+            if (!isMoving) {
+                if (Input.GetMouseButtonDown(0)) {
+                    Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    horizontal = worldToTile(mousePosition.x) - worldToTile(this.transform.position.x);
+                    vertical = worldToTile(mousePosition.y) - worldToTile(this.transform.position.y);
+                } else {
+                    horizontal = (int)Input.GetAxisRaw("Horizontal");
+                    vertical = (int)Input.GetAxisRaw("Vertical");
+                }
+
+                AttemptMove<Wall>(horizontal, vertical);
+            }
 			
 		}
 
 		protected override void AttemptMove<T> (int xDir, int yDir) {
-			Debug.Log ("AttemptMove");
+			Debug.Log ("AttemptMove: xdir: " + xDir + "yDir: " + yDir);
 			base.AttemptMove<T> (xDir, yDir);
 			RaycastHit2D hit;
 		}
