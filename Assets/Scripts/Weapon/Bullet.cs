93ed0e4f76006064fb39d34dc7e18f5961fd94f9
Andrew Manwaring <andrew.manwaring@gmail.com>
2017-03-16 22:19:36 -0600

    adjusted bullet trigger - not it doesn't get destroyed until it actually hits the wall (before, it would get destroyed once it was within the distance of whatever the 'size' of the bullet is

--- Assets/Scripts/Weapon/Bullet.cs
+++ Assets/Scripts/Weapon/Bullet.cs
@@ -6,6 +6,7 @@ public class Bullet : MonoBehaviour {
 
 	float maxSpeed = 7f;
     public float damage = 34f;
+    float scale = .5f;
 	
 	// Update is called once per frame
 	void Update () {
@@ -14,6 +15,30 @@ public class Bullet : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
-		Destroy (gameObject);
+        //Debug.Log("Bullet vector on impact: " + transform.position + " - hit: " + other.tag);
+
+        if (other.tag == "Enemy")
+		    Destroy (gameObject);
 	}
+
+    private void OnTriggerStay2D(Collider2D other) {
+        //This is probably not the best way to handle it, but
+        //This function is to take into account the fact that without it,
+        //Bullets get destroyed almost two tiles away from walls
+        //We ignore everything except enemies in OnTriggerEnter2D
+        //Then we keep checking the actual distance in OnTriggerStay2D, 
+        //And if it gets close enough we destroy the bullet
+        //Debug.Log("Sq distance: " + squareDist(transform.position, other.transform.position));
+        //It seems to need an additional margin of error of about .15
+        if (squareDist(transform.position, other.transform.position) <= (scale * scale - .15)) {
+            //Debug.Log("Bullet vector on impact: " + transform.position + " - hit: " + other.transform.position);
+            Destroy(gameObject);
+        }
+    }
+
+    private double squareDist(Vector2 a, Vector2 b) {
+        float xDist = a.x - b.x;
+        float yDist = a.y - b.y;
+        return xDist * xDist + yDist * yDist;
+    }
 }
